891e1b0e4a85538f182e2589b71594808cb2be7f
S515788 <S515788@NW26189.nwmissouri.edu>
2013-06-04 14:34:19 -0500

    Added Project

---
+++ src/kanagala/Kanagala.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kanagala;
+
+/**
+ *
+ * @author S515788
+ */
+public class Kanagala {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
